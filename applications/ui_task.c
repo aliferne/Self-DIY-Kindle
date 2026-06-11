d5f815619fefdbc55fbde12a3dcf04ce2dd82b2b
aliferne <a3230269751@163.com>
2026-06-11 23:00:02 +0800

    chore. 更新了 LVGL 借助读取的文件内容画 UI 的示例

--- applications/ui_task.c
+++ applications/ui_task.c
@@ -10,7 +10,7 @@
 #include "lv_demos.h"
 #include "disp_drv.h"
 
-char buf[256 * 100];
+char buf[20];
 
 void StartUITask(void const *argument)
 {
@@ -28,6 +28,27 @@ void StartUITask(void const *argument)
         // buf 里就是文件内容...
         lv_fs_close(&f);
     }
+
+    static lv_style_t style;
+    lv_style_init(&style);
+    lv_style_set_radius(&style, 5);
+
+    /*Make a gradient*/
+    lv_style_set_width(&style, 128);
+    lv_style_set_height(&style, LV_SIZE_CONTENT);
+
+    lv_style_set_pad_ver(&style, 0);
+    lv_style_set_pad_left(&style, 0);
+
+    lv_style_set_x(&style, lv_pct(0));
+    lv_style_set_y(&style, 0);
+
+    /*Create an object with the new style*/
+    lv_obj_t *obj = lv_obj_create(lv_scr_act());
+    lv_obj_add_style(obj, &style, 0);
+
+    lv_obj_t *label = lv_label_create(obj);
+    lv_label_set_text(label, buf);
 
     for (;;) {
         if (res == LV_FS_RES_OK)
--- docs/devlog/fs.md
+++ docs/devlog/fs.md
@@ -3,3 +3,9 @@ SDIO 的驱动还存在部分问题，主要是超时机制尚未加入， DMA �
 不过 FatFs 则基本功能均正常，这些已经测试过了
 
 现在存储服务也已经正常
+
+现在架构是
+
+storage_srv (service) -> lv_port_fs (middlewares) -> applications
+
+说实话其实我都不确定 storage_srv 是否应该保留，我总觉得实际上直接用 lvgl 的大而美文件系统就够了，反正也不会换库，不过写都写了不用太可惜了，看后面会不会造成负担吧，会的话再重构
